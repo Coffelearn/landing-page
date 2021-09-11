cd3f7e3862d41c0c325805f22f7fc5dc2c03c5a7
DevChristianWagner <christian.coffe@protonmail.com>
2021-09-11 18:29:40 -0300

    Initial GithubPages Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Navbar from './components/header/index'
+import Home from './components/home/index'
+import Aulas from './components/aulas/index'
+import Sobre from './components/sobre/index'
+import Duvida from './components/duvida/index'
+import Video from './components/video/index'
+import Contato from './components/contato/index'
+import Footer from './components/footer/index'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navigation">
+        <Navbar />
+      </div>
+
+      <Home />
+
+      <Aulas />
+
+      <Sobre />
+
+      <Duvida />
+
+      <Video />
+
+      <Contato />
+
+      <Footer />
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/header/entrar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './entrar.css'
+
+function Entrar() {
+    return (
+        <div className="entrar">
+            <a href="https://app.coffelear.com/entrar" target="_blank" rel="external">Entrar</a>
+        </div>
+    )
+}
+
+export default Entrar
